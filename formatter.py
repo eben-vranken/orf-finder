e070a72e10bff1be0dd6ae01087fb6c1868eceaf
eben-vranken <vranken.eben@outlook.com>
2026-07-05 17:16:12 +0200

    refactor: move codon counts to end of formatter

--- formatter.py
+++ formatter.py
@@ -1,8 +1,9 @@
 def print_codons(codons: dict[str, list]):
-    print(f"Found %d start codons" % len(codons["start"]))
     for i, _ in enumerate(codons["start"]):
         print(i, ":", codons["start"][i])
 
-    print(f"Found %d stop codons" % len(codons["stop"]))
     for i, _ in enumerate(codons["stop"]):
         print(i, ":", codons["stop"][i])
+
+    print(f"Found %d start codons" % len(codons["start"]))
+    print(f"Found %d stop codons" % len(codons["stop"]))

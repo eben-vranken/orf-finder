2b21ab14642aa4ab32b9c199401b724f37f83028
eben-vranken <vranken.eben@outlook.com>
2026-07-05 15:32:50 +0200

    feat: add start and end codon matching with index saving

--- reader.py
+++ reader.py
@@ -15,4 +15,22 @@ def load_sequence(file_location):
 if __name__ == "__main__":
     file = load_sequence("test_data/zika_thailand_2006_complete.fasta")
 
-    print(file)
+    codon_matching = False
+    indexes = []
+    current_index = []
+    for i, c in enumerate(file[:len(file)-2]):
+        codon = file[i] + file[i+1] + file[i+2]
+
+        if codon == "ATG" and not codon_matching:
+            codon_matching = True
+            current_index.append(i)
+        elif (codon == "TAA" or codon == "TAG" or codon == "TGA") and codon_matching:
+            codon_matching = False
+            current_index.append(i)
+            indexes.append(current_index)
+            current_index = []
+        
+        if codon_matching:
+            i += 2
+
+    print(indexes)
